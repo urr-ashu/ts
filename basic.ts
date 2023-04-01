3ae56767477e3e00be44646aa921adaf4c837f23
Ashutosh Kumar <ashutosh.kumar@smartfoodsafe.com>
2023-04-02 00:43:37 +0530

    initial commit

---
+++ basic.ts
@@ -0,0 +1,40 @@
+/**
+ * BASICS ABOUT TS...
+ * => Typescript is just a superset of JavaScript.
+ * => It is like a wrpper over the JS.
+ * => Adding strictness over JS.
+ * => Like how JAVA is a strictly typed language(which means we should specify the types...)
+ * => TS allows us to eliminate mistakes.
+ * => Sometimes in JS we used to get the error only at runtime those errors can be avoided by TS.
+ * => In simple words we can say TS is JS + strict declaration of TYPES.
+ * =>In the end of the day TS will be transpiled to JS only!
+ * => TS performs static checking on typing of code itself.
+ * => We should install typescript, since it's a kind of tool only most of the time it is installed as dev dependency
+ */
+
+/**
+ * TO RUN TS/ TO GET JS FROM JS - tsc file
+ */
+
+//CREATING VARIABLE - 
+let gst: number;
+let productName: string;
+let mrp: number = 100;
+let msp = 90;
+let retailers: string[];
+
+/**
+ * Concepts learned today - 
+ * => What is TS.
+ * => How to declare variables in TS. (of different types like string, number, array).
+ * => How to declare functions in TS.
+ * => Types in TS. (Creating a new type).
+ * => Optional properties in a type :?
+ * => How to declare allowed values.
+ * => | pipe symbol to use as or.
+ * => & sybbol to concat types.
+ * => enums in TS.
+ * => Interface in TS.
+ * 
+ */
+
